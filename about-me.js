a954d093450d3ecb9ff73a69d2fd359a59f27df5
nealchapman1995 <neal.chapman1@gmail.com>
2021-01-20 14:06:56 -0700

    adding projects page and increasing performence

--- about-me.js
+++ about-me.js
@@ -1,5 +1,7 @@
 const aboutMe = document.getElementById('about-me');
+const finalRow = document.getElementById('final-row');
 
 window.addEventListener('scroll',(event) => {
     aboutMe.style.opacity = 1;
+    finalRow.style.opacity = 1;
 });
